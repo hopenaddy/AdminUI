c01bd521358507262608f57d110c46514754ce65
Guest <guest-7kLkX5@CLASS36.(none)>
2014-11-03 17:09:34 +0200

    change stab to space

--- yaps/my_app/views.py
+++ yaps/my_app/views.py
@@ -6,26 +6,26 @@ def index(request):
     users_list = Users.objects.all().order_by('id')
     context = {'users_list': users_list}
     if request.POST['delete']:
-    	user_id = request.POST['delete']
-    	Users.objects.filter(id=user_id).delete()
+        user_id = request.POST['delete']
+        Users.objects.filter(id=user_id).delete()
     return render(request, 'my_app/index.html', context)
 
 def _user_save(request, user):
-	user.login = request.POST['login']
-	user.fullname = request.POST['fullname']
-	user.token = request.POST['token']
-	user.save()
+    user.login = request.POST['login']
+    user.fullname = request.POST['fullname']
+    user.token = request.POST['token']
+    user.save()
 
 def _send_page(request, user):
-	if "save" in request.POST:
-		_user_save(request, user)
-		return redirect('index')
-	return render(request, 'my_app/add.html', {'user': user})	
+    if "save" in request.POST:
+        _user_save(request, user)
+        return redirect('index')
+    return render(request, 'my_app/add.html', {'user': user})   
 
 def add(request):
-	user = Users()
-	return _send_page(request, user)
-	
+    user = Users()
+    return _send_page(request, user)
+    
 def edit(request, id):
-	user = Users.objects.get(id=id) 
-	return _send_page(request, user)
+    user = Users.objects.get(id=id) 
+    return _send_page(request, user)
