ff76a014ef9f9e563b46e7da7c1b92977e7a352b
alive-web <alivelp@gmail.com>
2014-11-09 20:05:25 +0200

    add test

--- my_app/tests.py
+++ my_app/tests.py
@@ -10,6 +10,7 @@ class IndexPageTest(TestCase):
     def test_userpage_available(self):
         request = self.factory.get("")
         response = index(request)
+        print response.content
         self.assertEqual(response.status_code, 200)
 
     def test_addpage_available(self):
@@ -20,11 +21,25 @@ class IndexPageTest(TestCase):
     def test_editpage_available(self):
         request = self.factory.get("")
         response = edit(request,1)
-        print response.content
-        self.assertEqual(response.status_code, 200)   
-         
+        self.assertEqual(response.status_code, 200)    
 
     def test_whatever_creation_mommy(self):
         what = mommy.make(Users)
         self.assertTrue(isinstance(what, Users))
+
+    def test_user_save(self):
+    	user=Users()
+    	request = self.factory.post("/users/", {"login":"test_user","fullname":"test_fullname","token":"test_token"})
+    	response = user_save(request, user)
+    	self.assertEqual(Users.objects.get(login="test_user").login, "test_user")    	    
     
+    def test_user_delete(self):
+        request = self.factory.post("/users/", {"delete":1})
+        response = index(request)
+        self.assertEqual(not Users.objects.filter(id=1), True)
+
+    def test_send_page(self):
+    	user=Users()
+        request = self.factory.post("/users/")
+        response = send_page(request, user)
+        self.assertEqual(response.content, add(request).content)    
--- my_app/views.py
+++ my_app/views.py
@@ -9,22 +9,22 @@ def index(request):
         Users.objects.filter(id=user_id).delete()
     return render(request, 'my_app/index.html', context)
 
-def _user_save(request, user):
+def user_save(request, user):
     user.login = request.POST['login']
     user.fullname = request.POST['fullname']
     user.token = request.POST['token']
     user.save()
 
-def _send_page(request, user):
+def send_page(request, user):
     if "save" in request.POST:
-        _user_save(request, user)
+        user_save(request, user)
         return redirect('index')
     return render(request, 'my_app/add.html', {'user': user})   
 
 def add(request):
     user = Users()
-    return _send_page(request, user)
+    return send_page(request, user)
     
 def edit(request, id):
     user = Users.objects.get(id=id) 
-    return _send_page(request, user)
+    return send_page(request, user)
