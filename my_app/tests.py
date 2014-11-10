3e91b9a0ed850103a16d8213dcf39a9939cd150e
Alive-web <alivelp@gmail.com>
2014-11-10 10:06:03 +0200

    changed test

--- my_app/tests.py
+++ my_app/tests.py
@@ -6,11 +6,11 @@ from model_mommy import mommy
 class IndexPageTest(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
+        self.user=Users()
 
     def test_userpage_available(self):
         request = self.factory.get("")
         response = index(request)
-        print response.content
         self.assertEqual(response.status_code, 200)
 
     def test_addpage_available(self):
@@ -21,25 +21,31 @@ class IndexPageTest(TestCase):
     def test_editpage_available(self):
         request = self.factory.get("")
         response = edit(request,1)
-        self.assertEqual(response.status_code, 200)    
+        self.assertEqual(response.status_code, 200)
 
     def test_whatever_creation_mommy(self):
         what = mommy.make(Users)
         self.assertTrue(isinstance(what, Users))
 
     def test_user_save(self):
-    	user=Users()
-    	request = self.factory.post("/users/", {"login":"test_user","fullname":"test_fullname","token":"test_token"})
-    	response = user_save(request, user)
-    	self.assertEqual(Users.objects.get(login="test_user").login, "test_user")    	    
-    
+        request = self.factory.post("/users/", {"login":"test_user",
+                                    "fullname":"test_fullname","token":"test_token"})
+        response = user_save(request, self.user)
+        self.assertEqual(Users.objects.get(login="test_user").login, "test_user")    
+        Users.objects.filter(login="test_user").delete()
+
     def test_user_delete(self):
-        request = self.factory.post("/users/", {"delete":1})
+        self.user.login="test_user"
+        self.user.fullname="test_fullname"
+        self.user.token="test_token"
+        self.user.save()
+        id = Users.objects.get(login="test_user").id
+        request = self.factory.post("/users/", {"delete":id})
         response = index(request)
-        self.assertEqual(not Users.objects.filter(id=1), True)
+        self.assertEqual(not Users.objects.filter(id=id), True)
 
     def test_send_page(self):
-    	user=Users()
+        user=Users()
         request = self.factory.post("/users/")
-        response = send_page(request, user)
-        self.assertEqual(response.content, add(request).content)    
+        response = send_page(request, self.user)
+        self.assertEqual(response.content, add(request).content)
