a909a302d36ddb4b7fa2dbe798e71cd941a6a16d
Alive-web <alivelp@gmail.com>
2014-11-13 15:07:41 +0200

    test

--- fabfile.py
+++ fabfile.py
@@ -7,8 +7,8 @@ def deploy():
     if not venv.exists():
         venv.create()
     venv.install()
-    put("adminUI/urls.py", ROOT+"/adminUI")
-    put("adminUI/settings.py", ROOT+"/adminUI")
+    put("adminUI/urls.py", "/opt/lv128/AdminUI/adminUI")
+    put("adminUI/settings.py", "/opt/lv128/AdminUI/adminUI")
     put("manage.py", ROOT)
     put("lv128_adminUI.service", ROOT)
     run("sudo mv %s/lv128_adminUI.service /etc/systemd/system/" % ROOT)
