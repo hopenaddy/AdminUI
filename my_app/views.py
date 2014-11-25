37fde5985ad81f9044441e65f22b66393ad28268
hopenaddy <na_5_ve@ukr.net>
2014-11-25 17:48:58 +0200

    changed

--- my_app/views.py
+++ my_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from my_app.models import Users, Tokens
+from models import Users, Tokens
 from django.contrib import auth
 
 def main_page(request):
@@ -7,29 +7,33 @@ def main_page(request):
 
 def index(request):
     users_list = Users.objects.all().order_by('id')
-    tokens_list = Tokens.objects.all.order_by('id')
+    tokens_list = Tokens.objects.all().order_by('id')
     context = {'users_list': users_list, 'tokens_list':tokens_list, 'username': auth.get_user(request).username}
     if "delete" in request.POST:
         user_id = request.POST['delete']
         Users.objects.filter(id=user_id).delete()
     return render(request, 'my_app/index.html', context)
 
-def user_save(request, user):
+def user_save(request, user, tokens):
     user.login = request.POST['login']
     user.fullname = request.POST['fullname']
-    Tokens.token = request.POST['token']
     user.save()
+    tokens.token = request.POST['token']
+    tokens.user_id = Users.objects.get(login=request.POST['login']).id
+    tokens.save()
 
-def send_page(request, user):
+def send_page(request, user, tokens):
     if "save" in request.POST:
-        user_save(request, user)
+        user_save(request, user, tokens)
         return redirect('index')
     return render(request, 'my_app/add.html', {'user': user, 'username': auth.get_user(request).username})
 
 def add(request):
     user = Users()
-    return send_page(request, user)
+    tokens=Tokens()
+    return send_page(request, user, tokens)
     
 def edit(request, id):
     user = Users.objects.get(id=id)
-    return send_page(request, user)
+    tokens = Tokens.objects.get(id=id)
+    return send_page(request, user, tokens)
