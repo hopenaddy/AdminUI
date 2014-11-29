a46034e6360304bae921dc1dc2823344909d80e7
alive-web <alivelp@gmail.com>
2014-11-29 19:43:41 +0200

    add logging

--- adminUI/settings.py
+++ adminUI/settings.py
@@ -105,14 +105,14 @@ LOGGING  =  {
         'default' :  { 
             'level' :  'DEBUG' , 
             'class' :  'logging.handlers.RotatingFileHandler' , 
-            'filename' :  'log/all_operation.log' ,
+            'filename' :  '../log/AdminUI/all_operation.log' ,
             'formatter' : 'standard',
 
         },
          'request_handler' :  { 
             'level' :  'DEBUG' , 
             'class' :  'logging.FileHandler' , 
-            'filename' :  'log/debug.log' , 
+            'filename' :  '../log/AdminUI/debug.log' , 
             'formatter' : 'standard',
         }, 
     }, 
