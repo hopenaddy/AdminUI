03996cc3323f5011b1cbd32e9b04ca3bbf5eb00a
Volodymyr <web.switch.ua@gmail.com>
2014-12-03 23:28:55 +0200

    Mysql connection added

--- adminUI/settings.py
+++ adminUI/settings.py
@@ -61,16 +61,16 @@ WSGI_APPLICATION = 'adminUI.wsgi.application'
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.mysql',
-        #'NAME': "yaps",
-        #'HOST':"localhost",
-        #'USER':"",
-        #'PASSWORD':"",
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "yaps",
+        'HOST':"localhost",
+        'USER':"",
+        'PASSWORD':"",
         #'PORT':""
         #'TEST_DEPENDENCIES': ['slave'],
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'TEST_MIRROR': 'default'
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'TEST_MIRROR': 'default'
     },
 }
 
