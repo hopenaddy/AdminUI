088131db425d81a67c8ab47c7241ca0053dc2fc5
Pavlo Levytskyi <alivelp@gmail.com>
2014-12-03 19:01:00 +0200

    Update settings.py

--- adminUI/settings.py
+++ adminUI/settings.py
@@ -107,14 +107,14 @@ LOGGING  =  {
         'default' :  { 
             'level' :  'DEBUG' , 
             'class' :  'logging.handlers.RotatingFileHandler' , 
-            'filename' :  '/opt/lv128/log/admin_all_operation.log' ,
+            'filename' :  'log/admin_all_operation.log' ,
             'formatter' : 'standard',
 
         },
          'request_handler' :  { 
             'level' :  'WARNING' , 
             'class' :  'logging.FileHandler' , 
-            'filename' :  '/opt/lv128/log/admin_debug.log' , 
+            'filename' :  'log/admin_debug.log' , 
             'formatter' : 'standard',
         }, 
     }, 
