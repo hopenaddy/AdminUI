ba0772e22ddcdab3c1d3aaaada77d7de6e2eb40f
alive-web <alivelp@gmail.com>
2014-12-08 20:42:31 +0200

    edit button add

--- my_app/urls.py
+++ my_app/urls.py
@@ -4,6 +4,5 @@ from my_app import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^add/$', views.add, name='add'),
     url(r'^edit/(?P<id>\d+)$', views.edit, name='edit')
 )
