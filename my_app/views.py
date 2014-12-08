40fa9804783edab05c8ab1f620c51d8070330df3
alive-web <alivelp@gmail.com>
2014-12-08 20:40:50 +0200

    edit button add

--- my_app/views.py
+++ my_app/views.py
@@ -20,36 +20,17 @@ def index(request):
         user_id = request.POST['delete']
         User.objects.filter(id=user_id).delete()
         logger.debug("%s delete user with id= %s" % (auth.get_user(request).username, user_id))
-    return render(request, 'my_app/index.html', context)
-    
-def user_save(request, user):
-    user.username = request.POST['login']
-    user.first_name = request.POST['first']
-    user.last_name = request.POST['last']
-    if not user.id:
-        user.save()
-        id=User.objects.last().id
-        profile = Profile()
-        profile.user_id=id 
-        profile.token = uuid.uuid4()
-        profile.save()
-        logger.debug("%s create user login= %s fullname= %s" % (auth.get_user(request).username, user.username, user.get_full_name()))
-    else:
-        logger.debug("%s edit user login= %s fullname= %s" % (auth.get_user(request).username, user.username, user.get_full_name()))
-        user.save()
-
-def send_page(request, user):
-    if "save" in request.POST:
-        user_save(request, user)
-        return redirect('index')
-    return render(request, 'my_app/add.html', {'user': user, 'username': auth.get_user(request).username})
-
-@login_required()
-def add(request):
-        user = User()
-        return send_page(request, user)
+    return render(request, 'my_app/index.html', context)    
 
 @login_required()    
 def edit(request, id):
-        user = User.objects.get(id=id)
-        return send_page(request, user)
+    user = User.objects.get(id=id)
+    if "save" in request.POST:
+        user.username = request.POST['login']
+        user.first_name = request.POST['first']
+        user.last_name = request.POST['last']
+        logger.debug("%s edit user login= %s fullname= %s" % (auth.get_user(request).username, user.username, user.get_full_name()))
+        user.save()
+        return redirect('index')
+    return render(request, 'my_app/add.html', {'user': user, 'username': auth.get_user(request).username})
+        
