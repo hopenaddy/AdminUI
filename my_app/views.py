48b73b3aab59335c6382b5a1c882dbd316c34d26
alive-web <alivelp@gmail.com>
2014-12-14 17:43:12 +0200

    now we can add and del tokens

--- my_app/views.py
+++ my_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from my_app.models import *
+from loginsys.views import add_token
 from django.core.urlresolvers import reverse
 import uuid
 from django.contrib.auth.models import User
@@ -27,15 +28,20 @@ def delete(request, id):
     return redirect(reverse('index'))     
 
 @login_required() 
-@permission_required("auth.change_user")
 def edit(request, id):
-    user = User.objects.get(id=id)
-    if "save" in request.POST:
-        user.username = request.POST['login']
-        user.first_name = request.POST['first']
-        user.last_name = request.POST['last']
-        logger.debug("%s edit user login= %s fullname= %s" % (auth.get_user(request).username, user.username, user.get_full_name()))
-        user.save()
-        return redirect('index')
-    return render(request, 'my_app/add.html', {'user': user, 'username': auth.get_user(request).username})
+    if request.user.has_perm('auth.change_user') or request.user.id==int(id):
+        user = User.objects.get(id=id)
+        if "del" in request.POST:
+            Profile.objects.filter(id=request.POST["del"]).delete()
+        if "add" in request.POST:
+            add_token(id)
+        if "save" in request.POST:
+            user.username = request.POST['login']
+            user.first_name = request.POST['first']
+            user.last_name = request.POST['last']
+            logger.debug("%s edit user login= %s fullname= %s" % (auth.get_user(request).username, user.username, user.get_full_name()))
+            user.save()
+            return redirect('index')
+        return render(request, 'my_app/add.html', {'user': user, 'username': auth.get_user(request).username})
+    return redirect('/auth/login/?next=%s' % request.path)    
         
